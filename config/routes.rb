cfd1711f9a5f7c818191c213af180300c28dce91
Jowitam <jowita.mielnicka@gmail.com>
2021-09-07 20:46:47 +0200

    ajax - linki asynchroniczne

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  resources :posts
+  resources :posts do
+    collection do
+      get :published
+      get :surprise
+    end
+  end
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
